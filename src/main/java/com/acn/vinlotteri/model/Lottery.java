0b887324dbd6c361ba4207a959998e0567c84b4a
Ragnhild Sand <ragnhild.evebo.sand@gmail.com>
2019-03-09 18:15:36 +0100

    Persist database to file

--- src/main/java/com/acn/vinlotteri/model/Lottery.java
+++ src/main/java/com/acn/vinlotteri/model/Lottery.java
@@ -1,29 +1,29 @@
 package com.acn.vinlotteri.model;
 
 
-import lombok.Builder;
-import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDateTime;
 
-@Builder
-@Data
 @Entity
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Lottery {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    @CreatedDate
-    private LocalDateTime createdDate;
+    private String createdDate;
     private Boolean open;
     private Long ticketCost;
     private String phoneNumber;
-
 
 }
