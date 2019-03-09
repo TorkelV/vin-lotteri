658cb5a1c9f3a779fbfc2b4498904c834107511b
TorkelV <torkel93@gmail.com>
2019-03-09 22:05:33 +0100

    plugins

---
+++ src/main/java/com/acn/vinlotteri/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.acn.vinlotteri;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(VinlotteriApplication.class);
+    }
+
+}
